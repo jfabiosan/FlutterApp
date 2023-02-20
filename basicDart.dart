635f37b9cd10d4787910670931bc90754124a280
Fabio <josefabiosan@gmail.com>
2023-02-19 21:54:03 -0300

    a base do dart

---
+++ basicDart.dart
@@ -0,0 +1,16 @@
+void main() {
+  int year = 2009;
+  String result = rateYear(year);
+  print(result);
+}
+
+String rateYear(int year) {
+  if (year <= 1999) {
+    return "carro muito velho";
+  } else if (year <= 2005) {
+    return "carro antigo";
+  } else if (year <= 2009) {
+    return "carro semi-novo";
+  }
+  return "carro do ano";
+}
