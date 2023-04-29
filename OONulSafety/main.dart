aa0489675a712679055b92eb7b3010b77fc87493
Fabio <josefabiosan@gmail.com>
2023-04-28 23:25:36 -0300

    Criando classes

---
+++ OONulSafety/main.dart
@@ -0,0 +1,9 @@
+import 'pessoa.dart';
+
+void main() {
+  Pessoa eleitor = Pessoa(
+    nome: "José",
+    sobrenome: "Santos",
+  );
+  print('O nome do eleitor é ${eleitor.nome} dos ${eleitor.sobrenome}');
+}
