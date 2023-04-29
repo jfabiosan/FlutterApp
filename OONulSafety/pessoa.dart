0a41881cc98de74c1473c8ddbaa584aa19706e51
Fabio <josefabiosan@gmail.com>
2023-04-28 23:25:50 -0300

    atributos e construtor

---
+++ OONulSafety/pessoa.dart
@@ -0,0 +1,8 @@
+//criando a classe Pessoa
+class Pessoa {
+  //criando os atributos
+  late String nome;
+  late String sobrenome;
+  //criando o construtor
+  Pessoa({required this.nome, required this.sobrenome});
+}
