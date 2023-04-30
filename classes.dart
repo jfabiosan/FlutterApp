cf62ec39d8ce0e8cabf2d6955ec6a8a0262774f8
Fabio <josefabiosan@gmail.com>
2023-04-29 21:50:54 -0300

    estudando os tipos de contrutores

--- classes.dart
+++ classes.dart
@@ -7,15 +7,15 @@ void main() {
   print(mycar.name);
   print(mycar.year);
   print(mycar.weight);
-  mycar.descriptions();
+  mycar.getvalue();
 }
 
 //Criando uma classe
 class Car {
   //Atributos
-  late String name;
-  late int year;
-  late double weight;
+  String name;
+  int year;
+  double weight;
 
   //Construtor posicional 1
   /*Car(String name, int year, double weight) {
@@ -32,8 +32,11 @@ class Car {
 
   //Construtor com valor default
   Car([this.name = 'Maria', this.year = 2000, this.weight = 998]);
+  void getvalue() {
+    _descriptions();
+  }
 
-  void descriptions() {
+  void _descriptions() {
     print(
         "Essa $name foi fabricada no ano de $year e tem o pesso de $weight kg");
   }
