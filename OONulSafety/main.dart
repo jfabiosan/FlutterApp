3be8f9defc207f5b319aa4fec1f5d2bf522499a3
Fabio <josefabiosan@gmail.com>
2023-04-29 23:47:16 -0300

    get e set

--- OONulSafety/main.dart
+++ OONulSafety/main.dart
@@ -2,8 +2,8 @@ import 'pessoa.dart';
 
 void main() {
   Pessoa eleitor = Pessoa(
-    nome: "José",
-    sobrenome: "Santos",
+    nome: 'José',
+    sobrenome: 'Santos',
   );
   print('O nome do eleitor é ${eleitor.nome} dos ${eleitor.sobrenome}');
 }
--- OONulSafety/pessoa.dart
+++ OONulSafety/pessoa.dart
@@ -3,6 +3,7 @@ class Pessoa {
   //criando os atributos
   String nome;
   String sobrenome;
+
   //criando o construtor
   Pessoa({required this.nome, required this.sobrenome});
 }
