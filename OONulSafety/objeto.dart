d77cfe700c0e4ea65b71bcd237cdd759e088d261
Fabio <josefabiosan@gmail.com>
2023-04-29 22:13:16 -0300

    exercicios de fixacao

---
+++ OONulSafety/objeto.dart
@@ -0,0 +1,13 @@
+void main() {
+  Moto biz = Moto('biz 125 ks', 'preta', 2006);
+  print('Comprei uma ${biz.modelo} na cor ${biz.cor} e ano ${biz.ano}.');
+}
+
+class Moto {
+  //? indica que pode ser nula
+  String? modelo;
+  String? cor;
+  int? ano;
+
+  Moto(this.modelo, this.cor, this.ano);
+}
